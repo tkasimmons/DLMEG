9ac6c83540425c7ed99f24778e5cce2b1c53e83d
tkasimmons <128121478+tkasimmons@users.noreply.github.com>
2024-07-18 15:22:07 +0900

    Add files via upload

---
+++ models.py
@@ -0,0 +1,19 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops.layers.torch import Rearrange
+import torchvision.models as models
+
+class Resnet34(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1d = nn.Conv1d(271,64,kernel_size=3,stride=1,padding=1)
+        self.resnet34 = models.resnet34()
+        self.resnet34.conv1 = nn.Conv2d(64, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.resnet34.fc = nn.Linear(self.resnet34.fc.in_features, 1854)
+
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
+        X = self.conv1d(X)
+        X = X.unsqueeze(2)
+        return self.resnet34(X)
